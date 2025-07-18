e71354129df3d044bdcfd15ba5385bdef58d4b06
Ashwin V <klprogrammer39@gmail.com>
2025-07-18 13:25:53 +0530

    User checking, delete product, basic cart and edit product feature completed

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,14 @@ var router = express.Router();
 var productHelpers=require('../helpers/product-helpers')
 var collections=require('../config/collections')
 const userHelpers=require('../helpers/user-helpers')
+const verifyLogin=(req,res,next)=>{
+  if(req.session.loggedIn){
+
+    next()
+  }else{
+    res.redirect('/login')
+  }
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let user=req.session.user
@@ -14,7 +22,12 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/login',function(req,res,next){
-  res.render('user/login')
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else{
+  res.render('user/login',{"loginErr":req.session.logginErr})
+  req.session.loginErr=false
+  }
 })
 router.get('/signup',(req,res)=>{
   res.render('user/signup')
@@ -34,6 +47,7 @@ router.post('/login',(req,res)=>{
       req.session.user=response.user
       res.redirect('/')
     }else{
+      req.session.logginErr="Invalid username or password"
       res.redirect('/login')
     }
   })
@@ -43,4 +57,12 @@ router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
+
+router.get('/cart',verifyLogin,(req,res)=>{
+  verifyLogin()
+  res.render('/cart')
+})
+
+
+
 module.exports = router;
