98baffce49e8bc052c77236e1f6726b6ff0a1543
Ashwin V <klprogrammer39@gmail.com>
2025-07-18 13:25:53 +0530

    User checking, delete product, basic cart and edit product feature completed

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,5 +1,6 @@
 var db=require('../config/connection')
 var collection=require('../config/collections.js')
+var objectId=require('mongodb').ObjectId
 
 module.exports={
     addProduct:(product,callback)=>{
@@ -13,6 +14,38 @@ module.exports={
         return new Promise(async(resolve,reject)=>{
             let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
             resolve(products)
+        })
+    },
+
+    deleteProduct:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            console.log(proId)
+            console.log(objectId(proId));
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objectId(proId)}).then((response)=>{
+            resolve(response)
+
+            })
+        })
+    },
+    getProductDetails:(proId)=>{
+        return new Promise(async(resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise(async(resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id:objectId(proId)},{
+                $set:{
+                    productName:proDetails.productName,
+                    productDescription:proDetails.productDescription,
+                    productPrice:proDetails.productPrice,
+                    Category:proDetails.Category,
+                }
+            }).then((response)=>{
+                resolve()
+            })
         })
     }
 
