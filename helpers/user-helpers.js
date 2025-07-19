263f84d73ccfa63ef18f84ebeaae927a89415033
Ashwin V <klprogrammer39@gmail.com>
2025-07-19 09:31:35 +0530

    add to cart with ajax done

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,6 +1,8 @@
 var db=require('../config/connection')
 var collection=require('../config/collections.js')
 const bcrypt=require('bcrypt')
+var objectId=require('mongodb').ObjectId
+
 module.exports={
     doSignup:(userData)=>{
         return new Promise(async(resolve,reject)=>{
@@ -37,5 +39,113 @@ module.exports={
 
                 }
         })
+    },
+    addToCart:(proId,userId)=>{
+        let proObj={
+            item:objectId(proId),
+            quantity:1
+        }
+        return new Promise(async(resolve,reject)=>{
+            let userCart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+            if(userCart){
+                let prodExist=userCart.products.findIndex(product=> product.item==proId)
+                if(prodExist!=-1){
+                    db.get().collection(collection.CART_COLLECTION)
+                    .updateOne({ 'products.item':objectId(proId) },
+                {
+                    $inc:{'products.$.quantity':1}
+                }
+                ).then(()=>{
+                    resolve()
+                })
+                }else[
+  db.get().collection(collection.CART_COLLECTION)
+                    .updateOne({user:objectId(userId)},
+                    {
+                        $push:{products:proObj}
+                    }
+                ).then((response)=>{
+                        resolve()
+                    })
+                ]
+                
+                  
+                
+            }else{
+                let cartObj={
+                    user:objectId(userId),
+                    products:[proObj]
+                }
+                db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response)=>{
+                    resolve()
+                })
+            }
+        })
+    },
+    getCartProducts:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let cartItems=await db.get().collection(collection.CART_COLLECTION).aggregate([
+                {
+                    $match:{user:objectId(userId)}
+                },
+                {
+                    $unwind:'$products'
+                },
+                {
+                    $project:{
+                        item:'$products.item',
+                        quantity:'$products.quantity'
+                    }
+                },
+                {
+                $lookup:{
+                    from:collection.PRODUCT_COLLECTION,
+                    localField:'item',
+                    foreignField:'_id',
+                    as:"product"
+                }
+           
+                },
+                {
+                    $project:{
+                        item:1,quantity:1,product:{$arrayElemAt:['$product',0]}
+                    }
+                }
+
+                // {
+                //     $lookup:{
+                //         from:collection.PRODUCT_COLLECTION,
+                //         let:{prodList:'$products'},
+                //         pipeline:[
+                //             {
+                //                 $match:{
+                //                     $expr:{
+                //                         $in:['$_id',"$$prodList"]
+                //                     }
+                //                 }
+                //             }
+                //         ],
+                //         as:'cartItems'
+
+                //     }
+                // }
+            ]).toArray()
+            resolve(cartItems)
+        })
+    },
+    getCartCount:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let count=0
+            let cart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+            
+            if(cart){
+                cart.products.forEach((product)=>{
+                        count+=product.quantity
+                })
+            }
+                            resolve(count)
+
+        })
     }
+
 }
